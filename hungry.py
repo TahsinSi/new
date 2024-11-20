975541a3277ac22b5125ab90fab0ea0ba159cc1d
unknown <tahsinsiddiki5@gmail.com>
2024-11-20 01:03:22 -0800

    problem

--- hungry.py
+++ hungry.py
@@ -1,1 +1,7 @@
-print("i need to wait to eat. UNTIL I get the food of my own , i can't eat")
+inp=input('Am i hungry to eat?')
+
+if inp=='yes':
+    print("i need to wait to eat. UNTIL I get the food of my own , i can't eat")
+else:
+    print("you will be punished by allah, if you eat food those doesn'y belong to you")
+
