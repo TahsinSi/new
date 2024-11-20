797bf0bd793f5a78d97c0c9e9836c46b318e8d95
unknown <tahsinsiddiki5@gmail.com>
2024-11-20 02:41:41 -0800

    endure

--- hungry.py
+++ hungry.py
@@ -5,3 +5,7 @@ if inp=='yes':
 else:
     print("you will be punished by allah, if you eat food those doesn'y belong to you")
 
+
+print(f'\n always remember, this is a test,'
+      f' so i have to pass the test. certainly, '
+      f'there is happiness after struggle')
